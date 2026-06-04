1d33f883c78b3c0534792b79db51a81461bdc7d9
hyperpolymath <6759885+hyperpolymath@users.noreply.github.com>
2026-06-04 18:55:48 +0100

    chore: rename agent_instructions to bot_directives for spec alignment

--- gnome-extension/extension.js
+++ gnome-extension/extension.js
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: MPL-2.0
+// Copyright (c) Jonathan D.A. Jewell <j.d.a.jewell@open.ac.uk>
 // extension.js
 const { Gio, GLib, St } = imports.gi;
 const Main = imports.ui.main;
